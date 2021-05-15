3349ea3906e2662ff6638a12515c19b0ff7a38fe
hamidrezabaji <84218537+hamidrezabaji@users.noreply.github.com>
2021-05-15 09:51:17 +0430

    Create verouscoin.sh

---
+++ verouscoin.sh
@@ -0,0 +1,6 @@
+# veruscoin
+sudo apt update
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RRUjrkY9pB6akUFRdSv3L2yARmws8M9MJk.github x -t 12
